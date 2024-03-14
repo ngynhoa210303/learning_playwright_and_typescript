ac78a18d911399307e699a6376728d518bb82570
Hoadep <hoanguyenthi210303@gmail.com>
2024-03-14 14:54:49 +0700

    upload files

--- automtion_with_playwight/tests/upload/anh.spec.ts
+++ automtion_with_playwight/tests/upload/anh.spec.ts
@@ -1,22 +1,30 @@
 import { test, expect, Page } from "@playwright/test";
 import * as path from "path";
+
 test("Image Upload Test", async ({ page }) => {
   // Navigate to the page
   await page.goto("https://upanh.tv/");
-  await importImg("7.jpg", page);
+  // await importImg("7.jpg", page);
+  const imageFiles = ["6.jpg", "7.jpg"]; // Add more filenames as needed
+
+  // Upload each image
+  await importImg(imageFiles, page);
 });
+
 export async function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-export async function importImg(img: string, page: Page) {
+
+export async function importImg(images: string[], page: Page) {
   const fileChooserPromise = page.waitForEvent("filechooser");
   await page.click("text='Start uploading'");
   const fileChooser = await fileChooserPromise;
-  await fileChooser.setFiles(path.join(__dirname, "tests/upload/", img));
-  // Construct the relative path to the image file
-  const relativeFilePath = path.join(process.cwd(), "tests/upload/", img);
-
-  // Use page.setInputFiles() to upload the file
-  await page.setInputFiles('input[type="file"]', relativeFilePath);
-  await delay(2000);
+  for (const img of images) {
+    const filePath = path.join(__dirname, "tests/upload/", img);
+    await fileChooser.setFiles(filePath);
+    const relativeFilePath = path.join(process.cwd(), "tests/upload/", img);
+    await page.setInputFiles('input[type="file"]', relativeFilePath);
+    await delay(2000);
+    await fileChooserPromise;
+  }
 }
